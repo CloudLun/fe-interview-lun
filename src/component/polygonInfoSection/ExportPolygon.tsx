db6d168a980198998ecc34344524306b9f5133cf
CloudLun <cloud830902@gmail.com>
2025-01-23 16:03:25 -0500

    style: add margin bottom on the export button

--- src/component/polygonInfoSection/ExportPolygon.tsx
+++ src/component/polygonInfoSection/ExportPolygon.tsx
@@ -8,7 +8,7 @@ const ExportPolygon = ({ polygon, exportPointsInPolygon }: Props) => {
     return (
         <button
             onClick={() => exportPointsInPolygon(polygon)}
-            className="py-2 w-full bg-[#007bff] hover:bg-[#edeef2] font-semibold text-[0.75rem] text-white hover:text-[#007bff] text-center rounded-lg cursor-pointer"
+            className="mb-2 py-2 w-full bg-[#007bff] hover:bg-[#edeef2] font-semibold text-[0.75rem] text-white hover:text-[#007bff] text-center rounded-lg cursor-pointer"
         >
             導出範圍內
         </button>
