6e5a8c931af62a29b1dd129fd99eed01cecabce6
CloudLun <cloud830902@gmail.com>
2025-01-23 22:06:26 -0500

    text: more specific on the export data button text

--- src/component/polygonInfoSection/ExportPolygon.tsx
+++ src/component/polygonInfoSection/ExportPolygon.tsx
@@ -10,7 +10,7 @@ const ExportPolygon = ({ polygon, exportPointsInPolygon }: Props) => {
             onClick={() => exportPointsInPolygon(polygon)}
             className="mb-2 py-2 w-full bg-[#007bff] hover:bg-[#edeef2] font-semibold text-[0.75rem] text-white hover:text-[#007bff] text-center rounded-lg cursor-pointer"
         >
-            導出範圍內
+            導出範圍內細胞數據
         </button>
     );
 }
